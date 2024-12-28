06938fbe0d05da804492079b6bb72bdd00862fa7
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2024-12-28 23:21:52 +0530

    Smallest and Largest Element from array.

--- Array.cpp
+++ Array.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
-int main(){
+int smallestNumber(int arr[])
+{
+    int smallest = INT_MAX;
+    // int size = sizeof(arr) / sizeof(int);
+    int size = 5;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+int highestNumber(int arr[])
+{
+    int highest = INT_MIN;
+    int size = 5;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] > highest)
+        {
+            highest = arr[i];
+        }
+    }
+    return highest;
+}
+
+int main()
+{
     int size = 5;
     int marks[size];
-    // int size = sizeof(marks) / sizeof(int);
 
-    //Inserting elements
-    for(int i = 0; i<size; i++){
-        cout << "insert Element At " << i << " Position : " ;
+    // Inserting elements
+    for (int i = 0; i < size; i++)
+    {
+        cout << "insert Element At " << i << " Position : ";
         cin >> marks[i];
     }
     cout << endl;
-    //Printing elements
-    for(int i = 0; i<size; i++){
+    // Printing elements
+    for (int i = 0; i < size; i++)
+    {
         cout << "Elemnt at " << i << " Position is : " << marks[i] << endl;
     }
+    // Smallest Element
+    cout << "Lowest Marks : " << smallestNumber(marks) << endl;
+    // Highest Element
+    cout << "Highest Marks : " << highestNumber(marks) << endl;
+    return 0;
 }
