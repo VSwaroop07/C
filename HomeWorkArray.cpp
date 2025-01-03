c14e0461982cf5a1b6d5ebf007cd15d645437b5e
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2025-01-03 22:18:19 +0530

    Q.3 Hashmap Created for unique value printing

--- HomeWorkArray.cpp
+++ HomeWorkArray.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <climits>
 #include <algorithm>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 // Array homework
 
-
-//Question One : Calculate SUM and Product of all numbers in array.
+// Question One : Calculate SUM and Product of all numbers in array.
 int sum(int arr[], int size)
 {
     int sum = 0;
@@ -26,8 +27,7 @@ long long product(int arr[], int size)
     return product;
 }
 
-
-//Question Two : Swap the max and min number from array.
+// Question Two : Swap the max and min number from array.
 void swapArray(int arr[], int size)
 {
     int min = INT_MAX;
@@ -48,27 +48,37 @@ void swapArray(int arr[], int size)
         }
     }
     swap(arr[minIndex], arr[maxIndex]);
-
 }
 
 // Question Three : Find the unique Value in Array.
-int unique(int arr[], int size){
-    for(int i =0; i<size; i++){
-        for(int j =0; j<size; j++){
-            if(arr[i] == arr[j]){
-
-            }
+int unique(int arr[], int size)
+{
+    unordered_map<int, int> freqMap;
+    for (int i = 0; i < size; i++)
+    {
+        freqMap[arr[i]]++; //Increament The count for arr[i]
+    }
+    // Printing a HashMap
+    // for(const auto& pair:freqMap){
+    //     cout<< pair.first << " " << pair.second << endl;
+    // }
+    //Printing Unique Values
+    cout << "Unique Values in array : ";
+    for(const auto& pair:freqMap){
+        if(pair.second == 1){
+            cout << pair.first << " ";
         }
-        
     }
 }
 
-int main(){
+int main()
+{
     int size = 10;
-    int arr[size] = {23,42,54,23,24,56,33,51,53,33};
+    int arr[size] = {23, 42, 54, 23, 24, 56, 33, 51, 53, 33};
     cout << "Arrya Before any opration : ";
-    for(int i = 0; i<size; i++){
-        cout<< arr[i] << " ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
     cout << endl;
     int Asum = sum(arr, size);
@@ -78,8 +88,11 @@ int main(){
 
     swapArray(arr, size);
     cout << "After Swapping Array : ";
-    for(int i = 0; i<size; i++){
-        cout<< arr[i] << " ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
     }
     cout << endl;
+
+    unique(arr, size);
 }
