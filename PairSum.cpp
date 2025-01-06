0a57c27eb59de65f6c3a9d48c43b6e488bbcd607
Swaroop Vyawahare <Vyawahareswarup19@gmail.com>
2025-01-06 18:43:05 +0530

    Optimal Solution for TwoPair Sum

--- PairSum.cpp
+++ PairSum.cpp
@@ -2,14 +2,17 @@
 #include <vector>
 using namespace std;
 
+// Brute Force Method
 vector<int> pairSum(vector<int> vec, int tar)
 {
     int size = vec.size();
     vector<int> ans;
     for (int i = 0; i < size; i++)
     {
-        for(int j = i+1; j<size; j++){
-            if((vec[i] + vec[j]) == tar){
+        for (int j = i + 1; j < size; j++)
+        {
+            if ((vec[i] + vec[j]) == tar)
+            {
                 ans.push_back(i);
                 ans.push_back(j);
                 return ans;
@@ -18,13 +21,48 @@ vector<int> pairSum(vector<int> vec, int tar)
     }
 }
 
-int main(){
-    vector<int> vec = {34,7,2,23};
+// Optimal Method *Requires Sorted Array
+vector<int> oPairSum(vector<int> vec, int tar)
+{
+    int size = vec.size();
+    vector<int> ans;
+    int st = 0;
+    int end = size - 1;
+    while (st < end)
+    {
+        int sum = vec[st] + vec[end];
+        if (sum > tar)
+        {
+            end--; // Move the end pointer left
+        }
+        else if (sum < tar)
+        {
+            st++; // Move the start pointer right
+        }
+        else
+        { // Pair found
+            ans.push_back(st);
+            ans.push_back(end);
+            return ans;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> vec = {2, 7, 22, 23};
     int tar = 9;
     vector<int> ans;
-    ans = pairSum(vec, tar);
-    for(int val : ans){
-        cout<<val;
+    ans = oPairSum(vec, tar);
+    for (int val : ans)
+    {
+        cout << val;
     }
+
+    // ans = pairSum(vec, tar);
+    // for(int val : ans){
+    //     cout<<val;
+    // }
+
     return 0;
 }
